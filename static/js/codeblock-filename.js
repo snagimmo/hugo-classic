59dbfabf748dc6261d74ca16d6eaecee41c1c629
snagimmo <31765952+snagimmo@users.noreply.github.com>
2021-02-09 12:53:45 +0900

    finished code blocks

--- static/js/codeblock-filename.js
+++ static/js/codeblock-filename.js
@@ -2,11 +2,13 @@ var list = document.body.getElementsByClassName("highlight");
 
 for(i=0; i <= list.length-1; i++){
   var code = list[i].firstElementChild.firstElementChild
-  var codeName =  code ? code.className.split(":")[1] : null;
+  if(code) {
+    var codeType =  code.className.split(":")[1] ? code.className.split(":")[1] : code.className.split(":")[0].replace(/language-/, '');
+  }
 
-  if(codeName) {
+  if(codeType) {
     var div = document.createElement('div');
-    div.textContent = codeName;
+    div.textContent = codeType;
     div.classList.add('code-name');
     code.parentNode.insertBefore(div, code);
   }
